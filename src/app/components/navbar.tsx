858bbcdc47b8c2fcc9f1f2634ab2810a25d3b9a0
Stefan <moldovanste@gmail.com>
2024-11-01 13:44:00 +0200

    Update navbar.tsx

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -21,7 +21,7 @@ const Navbar: React.FC<Props> = () => {
               <button
                 type="button"
                 onClick={toggleMobileMenu}
-                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-950 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-[#2b2b2b] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                 aria-controls="mobile-menu"
                 aria-expanded={isMobileMenuOpen}
               >
