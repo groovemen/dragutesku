4af8af89e8a56d4b8ec32f1af060f06fc0161b73
Stefan <moldovanste@gmail.com>
2024-11-07 15:58:04 +0200

    updates paralax and navbar

--- src/app/components/parallaxBottom.tsx
+++ src/app/components/parallaxBottom.tsx
@@ -5,12 +5,9 @@ type Props = {};
 const ParallaxBottom = (props: Props) => {
   return (
     <>
-      <div className="relative h-[33rem] w-full bg-[url('/tools.png')] bg-fixed bg-center bg-cover mt-16">
+      <div className="relative h-[33rem] w-full bg-[url('/tools.png')] bg-fixed bg-center bg-cover">
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40"></div>
       </div>
-      <br/>
-      <br/>
-      <br/>
     </>
   );
 };
