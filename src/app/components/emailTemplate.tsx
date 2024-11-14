927441d722cf8a91640479032fa23d1f1f97b7db
Stefan <moldovanste@gmail.com>
2024-11-14 12:18:29 +0200

    Update emailTemplate.tsx

--- src/app/components/emailTemplate.tsx
+++ src/app/components/emailTemplate.tsx
@@ -1,3 +1,4 @@
+// app/components/emailTemplate.tsx
 import React from 'react';
 
 type EmailTemplateProps = {
@@ -24,20 +25,18 @@ export const EmailTemplate: React.FC<EmailTemplateProps> = ({
   country,
   projectinfo,
   optionsSelected,
-}) => {
-  return (
-    <div style={{ fontFamily: 'Lato, regular', color: '#333' }}>
-      <h1>New Order Request - DrgMastering</h1>
-      <p><strong>Company:</strong> {company}</p>
-      <p><strong>Full Name:</strong> {fullname}</p>
-      <p><strong>Email:</strong> {email}</p>
-      <p><strong>VAT ID:</strong> {vat}</p>
-      <p><strong>Address:</strong> {address}</p>
-      <p><strong>City:</strong> {city}</p>
-      <p><strong>Postal Code:</strong> {postalcode}</p>
-      <p><strong>Country:</strong> {country}</p>
-      <p><strong>Project Information:</strong> {projectinfo}</p>
-      <p><strong>Services Selected:</strong> {optionsSelected}</p>
-    </div>
-  );
-};
+}) => (
+  <div style={{ fontFamily: 'Lato, sans-serif', color: '#333' }}>
+    <h1>New Order Request - DrgMastering</h1>
+    <p><strong>Company:</strong> {company}</p>
+    <p><strong>Full Name:</strong> {fullname}</p>
+    <p><strong>Email:</strong> {email}</p>
+    <p><strong>VAT ID:</strong> {vat}</p>
+    <p><strong>Address:</strong> {address}</p>
+    <p><strong>City:</strong> {city}</p>
+    <p><strong>Postal Code:</strong> {postalcode}</p>
+    <p><strong>Country:</strong> {country}</p>
+    <p><strong>Project Information:</strong> {projectinfo}</p>
+    <p><strong>Services Selected:</strong> {optionsSelected}</p>
+  </div>
+);
