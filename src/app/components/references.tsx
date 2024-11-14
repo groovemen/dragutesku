7a379458d26859b4d88b47a642ddcdb5a566e359
Stefan <moldovanste@gmail.com>
2024-11-14 14:26:56 +0200

    Update references.tsx

--- src/app/components/references.tsx
+++ src/app/components/references.tsx
@@ -33,6 +33,7 @@ const References = ({ references }: Props) => {
                     width={200}
                     height={200}
                     alt={reference.referenceAlt}
+                    loading="eager"
                   />
                   <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-hover:block">
                     <svg
