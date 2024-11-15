5a96943d7dc8068062bd1c4d1f4d99e5df4f13e1
Stefan <moldovanste@gmail.com>
2024-11-15 16:52:27 +0200

    Create page.tsx

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import PrivacyPolicy from "../components/policy"
+import Carousel from "../components/carousel";
+
+export default function PrivacyAndPolicy() {
+
+  return (
+    <main className="bg-white">
+      <Carousel />
+      <PrivacyPolicy />
+    </main>
+  );
+}
