f583a1452e132288e4ebef77897b8a2e48f4b2f2
Stefan <moldovanste@gmail.com>
2024-11-18 15:33:23 +0200

    terms pages + policy

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import PrivacyPolicy from "../components/policy"
-import Carousel from "../components/carousel";
+
 
 export default function PrivacyAndPolicy() {
 
   return (
     <main className="bg-white">
-      <Carousel />
+
       <PrivacyPolicy />
     </main>
   );
--- src/app/terms-conditions/page.tsx
+++ src/app/terms-conditions/page.tsx
@@ -2,12 +2,10 @@
 
 import Image from "next/image";
 import TermsConditions from "../components/terms"
-import Carousel from "../components/carousel";
 
 export default function TermsAndConditions() {
   return (
-    <main className="bg-white">
-      <Carousel />
+    <main className="container mx-auto pt-24">
       <TermsConditions />
     </main>
   );
